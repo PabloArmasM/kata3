c98923945f8274daf4f60bbaa4312808a3de4847
Pablo Armas Martín <>
2015-10-22 10:19:42 +0100

    Version 2  kata3

--- src/kata3/HistogramaDisplay.java
+++ src/kata3/HistogramaDisplay.java
@@ -1,52 +1,49 @@
 package kata3;
 
-
 import java.awt.Container;
 import java.awt.Dimension;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
-public class HistogramaDisplay extends ApplicationFrame {
+public class HistogramaDisplay extends ApplicationFrame{
 
-    public HistogramaDisplay() {
+    private final Histogram<String> histo;
+
+    public HistogramaDisplay(Histogram<String> histo) {
         super("HISTOGRAMA");
+        this.histo = histo;
         setContentPane(createPanel());
         pack();
     }
-
-    public void execute() {
-        setVisible(true);
-    }
+    
+    
 
     private ChartPanel createPanel() {
         ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
-        chartPanel.setPreferredSize(new Dimension(500, 450));
+        chartPanel.setPreferredSize(new Dimension(500,450));
         return chartPanel;
     }
-
-    private JFreeChart createChart(DefaultCategoryDataset dataset) {
-        JFreeChart chart = ChartFactory.createBarChart("Histogram de dominios", "DOMINIOS", "Nº email", dataset, PlotOrientation.VERTICAL, true, true, false);
+    
+    private JFreeChart createChart (DefaultCategoryDataset dataset){
+        JFreeChart chart = ChartFactory.createBarChart("Histograma de Dominios", "Dominios", "Nº de correos", dataset, PlotOrientation.VERTICAL, false, true, false);
         return chart;
     }
-
-    private DefaultCategoryDataset createDataset() {
+    
+    private DefaultCategoryDataset createDataset(){
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(10, "ENE", "gmail.com");
-        dataset.addValue(7, "ENE", "ulpgc.es");
-        dataset.addValue(4, "ENE", "ull.es");
-        dataset.addValue(8, "ENE", "hotmail.com");
-        dataset.addValue(1, "FEB", "gmail.com");
-        dataset.addValue(0, "FEB", "ulpgc.es");
-        dataset.addValue(-10, "FEB", "ull.es");
-        dataset.addValue(6, "FEB", "hotmail.com");
-        dataset.addValue(20, "MAR", "gmail.com");
-        dataset.addValue(15, "MAR", "ulpgc.es");
-        dataset.addValue(8, "MAR", "ull.es");
-        dataset.addValue(2, "MAR", "hotmail.com");
+        
+        for (String key : histo.keySet()) {
+            dataset.addValue(histo.get(key), "", key);
+        }
         return dataset;
+    }
+    
+    public void execute(){
+        setVisible(true);
     }
 }
